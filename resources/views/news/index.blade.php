d3a31dc7fad56d446efe265d794b582b4b753d63
dusankaK <karacdusanka11@gmail>
2019-11-10 09:58:23 +0100

    Deveti zadatak - dodata paginacija

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -5,7 +5,13 @@
     
     @foreach ($news as $vest)
 
-        <p> <a href="/news/{{ $vest->id }}">{{ $vest->title }}</a> posted by {{ $vest->user->name }} </p>
+        <p><a href="/news/{{ $vest->id }}">{{ $vest->title }}</a> posted by {{ $vest->user->name }}</p>
         
     @endforeach
+
+    <nav class="blog-pagination">
+            <a class="btn btn-outline-{{ $news->currentPage() == 1 ? 'secondary disabled' : 'primary' }}" href="{{ $news->previousPageUrl() }}">Older</a>
+            <a class="btn btn-outline-{{ $news->hasMorePages() ? 'primary' : 'secondary disabled'}}" href="{{ $news->nextPageUrl() }}">Newer</a>
+            Page {{ $news->currentPage() }} of {{ $news->lastPage() }}
+    </nav>
 @endsection
